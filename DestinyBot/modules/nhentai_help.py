b7f6940ac2029ff2d044e9db581ad3b2f37c1e89
HYBRID FREAK <hybridfreakk@gmail.com>
2022-05-24 21:44:20 +0530

    Update nhentai_help.py

--- DestinyBot/modules/nhentai_help.py
+++ DestinyBot/modules/nhentai_help.py
@@ -1,17 +1,17 @@
-__mod_name__ = "✟ Nhentai ✟"
+__mod_name__ = "Nhentai"
 
 __help__ = """
 
 *Nhentai / Doujins*
 
-✟ /nhentai <doujin code>*:* To get a doujin for the provided code.
+/nhentai <doujin code>*:* To get a doujin for the provided code.
 
-✟ /nhentai random*:* It provides random doujins irrespective or type and language.
+/nhentai random*:* It provides random doujins irrespective or type and language.
 
 Note:
 
 1. This will only work if you are in nsfw mode either in group or in private chat. do /addnsfw to enable nsfw mode before using it.
 
-2. If you need doujin codes, you can visit [Hentai Hostel](https://t.me/HunterCommittee/161).
+2. If you want to read doujins then just join [Doujinshi ALFA](https://t.me/Doujinshi_Alfa).
 
 """
